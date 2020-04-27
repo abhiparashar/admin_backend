9dee8a03ac0b5fc6795b8ddba0f67612ab7dc346
abhiparashar <aparashar103@gmail.com>
2020-04-27 15:36:42 +0530

    signin

--- controller/auth.js
+++ controller/auth.js
@@ -1,4 +1,4 @@
-exports.signup = () => {
+exports.signup = (req,res) => {
     const{name,password} = req.body
     res.json({
         user:{name,password}
--- models/user.js
+++ models/user.js
@@ -1,28 +1,30 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    userName:{
-        type:String,
-        trim:true,
-        required:true,
-        max:32,
-        unique:true,
-        index:true,
-        lowerase:true
-    },
-    hashed_password:{
-        type:String,
-        required:true
-    },
-    salt:Number,
-    role:{
-        type:Number,
-        trim:true
-    },
-    resetPasswordLink:{
-        data:String,
-        default:""
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+            length: 32,
+            unique: true,
+            index: true,
+            lowercase: true
+        },
+        hashed_password: {
+            type: String,
+            required: true
+        },
+        salt: String,
+        role: {
+            type: Number,
+            trim: true
+        },
+        resetPasswordLink: {
+            type: String,
+            default: ""
+        }
     }
-})
+);
 
-module.exports = mongoose.model('User',userSchema)
+module.exports = mongoose.model("User", userSchema);
